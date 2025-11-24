89df602d971e5e9bbc846b0704379f370df1d68a
SaraMelo <111928666+sararooberta@users.noreply.github.com>
2025-11-24 10:24:51 -0300

    Update questao_08.py

--- questao_08.py
+++ questao_08.py
@@ -1,1 +1,20 @@
+def f(nums):
+    n = len(nums)
+    resultado = [1] * n
+    
+    prefixo = 1
+    for i in range(n):
+        resultado[i] = prefixo
+        prefixo *= nums[i]
+    
+    sufixo = 1
+    for i in range(n-1, -1, -1):
+        resultado[i] *= sufixo
+        sufixo *= nums[i]
+    
+    return resultado
+
+#Utilizando os exemplos da questão
+print(f([1,2,4,6]))
+print(f([-1,0,1,2,3]))
 
