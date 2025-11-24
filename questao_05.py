a903839fd83e3fec111193dc70685ad51c79e40e
SaraMelo <111928666+sararooberta@users.noreply.github.com>
2025-11-24 10:22:02 -0300

    Update questao_05.py

--- questao_05.py
+++ questao_05.py
@@ -1,2 +1,8 @@
+def f(x, y):
+    resultado = []
+    for a, b in zip(x, y):
+        resultado.extend([a, b])
+    return resultado
 
 #Utilizando os exemplos da questão
+print(f(["a", "b", "c"], [1, 2, 3]))
