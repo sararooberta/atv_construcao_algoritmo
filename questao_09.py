68255748b810c7f5f66a9c6633308a7472845841
SaraMelo <111928666+sararooberta@users.noreply.github.com>
2025-11-24 10:25:14 -0300

    Update questao_09.py

--- questao_09.py
+++ questao_09.py
@@ -1,1 +1,12 @@
+def f(prices):
+    min_price = float('inf')
+    max_profit = 0
+    for p in prices:
+        min_price = min(min_price, p)
+        max_profit = max(max_profit, p - min_price)
+    return max_profit
+
+#Utilizando os exemplos da questão
+print(f([10,1,5,6,7,1]))
+print(f([10,8,7,5,2]))
 
