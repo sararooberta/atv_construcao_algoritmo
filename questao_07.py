ca49189e1c7fccd37da7fdb00378b9d056039a5a
SaraMelo <111928666+sararooberta@users.noreply.github.com>
2025-11-24 10:24:28 -0300

    Update questao_07.py

--- questao_07.py
+++ questao_07.py
@@ -1,1 +1,7 @@
+def f(x, y):
+    import numpy as np
+    coef = np.polyfit(x, y, 1)
+    return coef.tolist()
 
+#Utilizando os exemplos da questão
+print(f([-2, -2, -1, -1, 0, 0, 1, 1, 2, 2], [0, 0, 2, 3, 4, 4, 5, 6, 8, 8]))
